2f650e5c56a33273ba4fce78d9cff89af57f5173
Mayra <m.alvarez0598@gmail.com>
2024-01-20 13:56:39 -0600

    homeContainer terminado

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
+import HomeContainer from "./HomeContainer";
 
 const MainContainer = () => {
     return (
-        <div>
+        <div className="w-full h-auto flex flex-col items-center justify-center">
+            <HomeContainer />
         </div>
-    )
-}
+    );
+};
 
-export default MainContainer
+export default MainContainer;
---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import I1 from "../img/postres/i1.png";
+import F1 from "../img/frutas/f1.png";
+import C1 from "../img/carne/c3.png";
+import Fi1 from "../img/pescado/fi1.png";
+
+export const heroData = [
+    {
+        id : 1,
+        name : "Helado",
+        decp : "Chocolate & vainilla",
+        price : "65 MXN",
+        imageSrc : I1
+    },
+    {
+        id : 2,
+        name : "Fresas",
+        decp : "Fresas Frescas",
+        price : "70 MXN",
+        imageSrc : F1
+    },
+    {
+        id : 3,
+        name : "Kebab Pollo",
+        decp : "Kebab mixto Pollo",
+        price : "140 MXN",
+        imageSrc : C1
+    },
+    {
+        id : 4,
+        name : "Kebab Pescado",
+        decp : "Kebab mixto Pescado",
+        price : "150 MXN",
+        imageSrc : Fi1
+    },
+]
